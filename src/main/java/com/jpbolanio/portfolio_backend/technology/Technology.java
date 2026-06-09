00691d660e9347b4d85d7d0b69d57bef5f2a9d57
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-06-08 21:17:42 -0300

    fix: agregar anotación @Id a la clase Technology

--- src/main/java/com/jpbolanio/portfolio_backend/technology/Technology.java
+++ src/main/java/com/jpbolanio/portfolio_backend/technology/Technology.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -25,6 +26,7 @@ import lombok.Setter;
 @Builder
 public class Technology {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID uuid;
 
